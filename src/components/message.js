ffe855de46ebfbc5ea3298d40892caa1b80b08a6
ihargit <ihar.kharashun@gmail.com>
2019-08-13 21:20:24 +0300

    fix: right message position

--- src/components/message.js
+++ src/components/message.js
@@ -1,38 +1,57 @@
 import React from "react";
 import Paper from '@material-ui/core/Paper';
-import { Typography } from '@material-ui/core';
+import { Typography, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
     margin: theme.spacing(1),
-    padding: theme.spacing(1),
-    width: '80%',
+    maxWidth: '80%',
   },
+  messageBody: {
+    padding: theme.spacing(1),
+  },
+  messageTitle: {
+    display: 'flex',
+    // justifyContent: 'flex-end',
+    alignItems: 'baseline',
+  }
 }));
 
 export default function Message(props) {
   const classes = useStyles();
   return (
-    <Paper className={classes.root} style={{alignSelf: props.alignSelf}}>
-      <Typography
-        variant="body1"
-        style={{ marginRight: 10, fontWeight: 'bold', opacity: 0.7 }}
-      >
-        {props.from}
-      </Typography>
-      <Typography
-        variant="body2"
-        style={{ opacity: 0.5 }}
-        gutterBottom
-      >
-        {props.time}
-      </Typography>
-      <Typography
-        variant="body1"
-      >
-        {props.message}
-      </Typography>
-    </Paper>
+    <Box className={classes.root} style={{ alignSelf: props.flexEnd }} >
+      <Box className={classes.messageTitle} style={{ justifyContent: props.flexEnd }}>
+        <Typography
+          variant="body1"
+          display='inline'
+          style={{
+            marginRight: 10,
+            fontWeight: 'bold',
+            opacity: 0.7
+          }}
+        >
+          {props.from}
+        </Typography>
+        <Typography
+          variant="caption"
+          display='inline'
+          style={{
+            opacity: 0.5
+          }}
+          gutterBottom
+        >
+          {props.time}
+        </Typography>
+      </Box>
+      <Paper className={classes.messageBody}>
+        <Typography
+          variant="body1"
+        >
+          {props.message}
+        </Typography>
+      </Paper>
+    </Box>
   )
 }

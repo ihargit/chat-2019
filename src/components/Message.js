a7a81fcce0f58b7614c118869f234d1e9e3af044
ihargit <ihar.kharashun@gmail.com>
2019-08-15 23:56:20 +0300

    fix: change messages title width

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,55 +5,56 @@ import { makeStyles } from '@material-ui/core/styles';
 import { convertEpochDate } from '../utils';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    margin: theme.spacing(2),
-    maxWidth: '80%',
-  },
-  messageBody: {
-    padding: theme.spacing(1),
-    width: 'fit-content',
-    maxWidth: 'fill-available',
-  },
-  messageTitle: {
-    display: 'flex',
-    alignItems: 'baseline',
-  }
+    root: {
+        margin: theme.spacing(2),
+        maxWidth: '80%',
+    },
+    messageBody: {
+        padding: theme.spacing(1),
+        width: 'fit-content',
+        maxWidth: 'fill-available',
+    },
+    messageTitle: {
+        display: 'flex',
+        alignItems: 'baseline',
+    }
 }));
 
 export default function Message(props) {
-  const classes = useStyles();
-  return (
-    <Box component="li" className={classes.root} style={{ alignSelf: props.flexEnd }} >
-      <Box className={classes.messageTitle} style={{ justifyContent: props.flexEnd }}>
-        <Typography
-          variant="body1"
-          display='inline'
-          style={{
-            marginRight: 10,
-            fontWeight: 'bold',
-            opacity: 0.7
-          }}
-        >
-          {props.from}
-        </Typography>
-        <Typography
-          variant="caption"
-          display='inline'
-          style={{
-            opacity: 0.5
-          }}
-          gutterBottom
-        >
-          {convertEpochDate(props.time)}
-        </Typography>
-      </Box>
-      <Paper className={classes.messageBody}>
-        <Typography
-          variant="body1"
-        >
-          {props.message}
-        </Typography>
-      </Paper>
-    </Box>
-  )
+    const classes = useStyles();
+    return (
+        <Box component="li" className={classes.root} style={{ alignSelf: props.flexEnd }} >
+            <Box className={classes.messageTitle} style={{ justifyContent: props.flexEnd }}>
+                <Typography
+                    variant="body1"
+                    display='inline'
+                    style={{
+                        marginRight: 10,
+                        fontWeight: 'bold',
+                        opacity: 0.7,
+                        maxWidth: '70%',
+                    }}
+                >
+                    {props.from}
+                </Typography>
+                <Typography
+                    variant="caption"
+                    display='inline'
+                    style={{
+                        opacity: 0.5
+                    }}
+                    gutterBottom
+                >
+                    {convertEpochDate(props.time)}
+                </Typography>
+            </Box>
+            <Paper className={classes.messageBody}>
+                <Typography
+                    variant="body1"
+                >
+                    {props.message}
+                </Typography>
+            </Paper>
+        </Box>
+    )
 }
